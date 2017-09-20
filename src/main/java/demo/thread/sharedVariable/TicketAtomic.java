f073305978adb7bb47b296b5292047f0ea14121c
wanglei <wanglei0919@minxinjituan.cn>
2017-09-20 16:02:17 +0800

    thread

--- src/main/java/demo/thread/sharedVariable/TicketAtomic.java
+++ src/main/java/demo/thread/sharedVariable/TicketAtomic.java
@@ -3,26 +3,27 @@ package demo.thread.sharedVariable;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * @Title:抢票
- * @Description: 不使用同步代码块实现线程安全，会出现超卖
+ * @Title:抢票-原子变量
+ * @Description: 使用原子变量实现线程安全
  * @author lei.wang
  * @date 2017年9月20日 下午1:54:50
  * @version v1.0
  */
-public class Ticket implements Runnable {
+public class TicketAtomic implements Runnable {
 	// 共享变量
-	private int ticket = 10;
-	private AtomicInteger count = new AtomicInteger(0);
+	private AtomicInteger ticket = new AtomicInteger(10);
+	private AtomicInteger count = new AtomicInteger(0);// 纯计数的操作可以保证数据正确
 
 	@Override
 	public void run() {
 		for (int i = 0; i < 10; i++) {
-			if (this.ticket > 0) {
+			// 当出现竞态条件（if判断）时，原子变量一样不能保证线程安全
+			if (this.ticket.get() > 0) {
 				try {
 					// 通过睡眠线程来模拟出最后一张票的抢票场景
 					Thread.sleep(100);
 					System.out.println("次数：" + count.incrementAndGet() + "：" + Thread.currentThread().getName()
-							+ "卖票---->" + (this.ticket--));
+							+ "卖票---->" + (this.ticket.decrementAndGet())); // 纯计数的操作可以保证数据正确
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -31,7 +32,7 @@ public class Ticket implements Runnable {
 	}
 
 	public static void main(String[] arg) {
-		Ticket ticket = new Ticket();
+		TicketAtomic ticket = new TicketAtomic();
 		for (int i = 0; i < 4; i++) {
 			new Thread(ticket, "线程" + (i + 1) + "：").start();
 		}
--- src/main/java/demo/thread/sharedVariable/TicketSync.java
+++ src/main/java/demo/thread/sharedVariable/TicketSync.java
@@ -1,15 +1,18 @@
 package demo.thread.sharedVariable;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
-* @Title: 抢票
-* @Description: 使用同步代码块实现线程安全
-* @author lei.wang
-* @date 2017年9月20日 下午1:54:50 
-* @version v1.0
+ * @Title: 抢票
+ * @Description: 使用同步代码块实现线程安全
+ * @author lei.wang
+ * @date 2017年9月20日 下午1:54:50
+ * @version v1.0
  */
 public class TicketSync implements Runnable {
 	// 共享变量
 	private int ticket = 10;
+	private AtomicInteger count = new AtomicInteger(0);
 
 	@Override
 	public void run() {
@@ -20,7 +23,8 @@ public class TicketSync implements Runnable {
 					try {
 						// 通过睡眠线程来模拟出最后一张票的抢票场景
 						Thread.sleep(100);
-						System.out.println(Thread.currentThread().getName() + "卖票---->" + (this.ticket--));
+						System.out.println("次数：" + count.incrementAndGet() + "：" + Thread.currentThread().getName()
+								+ "卖票---->" + (this.ticket--));
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
@@ -28,11 +32,11 @@ public class TicketSync implements Runnable {
 			}
 		}
 	}
-	
-	public static void main(String[] arg){
+
+	public static void main(String[] arg) {
 		TicketSync ticket = new TicketSync();
-		for(int i=0 ; i < 2 ; i++){
-			new Thread(ticket, "线程" + (i+1) + "：").start();
+		for (int i = 0; i < 4; i++) {
+			new Thread(ticket, "线程" + (i + 1) + "：").start();
 		}
 	}
 }

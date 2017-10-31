0823c6eeb75a01b3f44a186b8e2c9430208a47a8
wanglei <wanglei0919@minxinjituan.cn>
2017-10-31 18:01:04 +0800

    动态代理

--- src/main/java/demo/designMode/factory/abstractFactory/Demo.java
+++ src/main/java/demo/designMode/factory/abstractFactory/Demo.java
@@ -1,5 +1,13 @@
 package demo.designMode.factory.abstractFactory;
 
+/**
+ * 抽象工厂模式
+* @ClassName:  Demo
+* @Description: 工程师装机
+* @author lei.wang
+* @date 2017年10月31日 下午5:49:22 
+* @version v1.0
+ */
 public class Demo {
 	public static void main(String[] args) {
 		ComputerEngineer ce = new ComputerEngineer();
--- src/main/java/demo/proxy/JdkDynamicProxy.java
+++ src/main/java/demo/proxy/JdkDynamicProxy.java
@@ -2,7 +2,7 @@ package demo.proxy;
 
 /**
 * @Title: java动态代理
-* @Description: TODO
+* @Description: java动态代理
 * @author lei.wang
 * @date 2017年10月25日 上午10:51:39 
 * @version v1.0
